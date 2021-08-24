b27ba6e68ceb2955f44d8ffeb0f669195dd61c0a
MK-NG <michaeljfknight@gmail.com>
2021-08-24 16:49:37 +0100

    add scheme pages

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service Name',
+  serviceName: 'Find schemes for your business ',
 
   // Default port that prototype runs on
   port: '3000',
